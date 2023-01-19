a9113dde4285b61d2cddff156cc8e8aa03ff12e9
Артём <coshurin.art@yandex.ru>
2023-01-19 19:21:34 +0300

    добавлен urls

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def index(request):
+    return HttpResponse('Главная страница. '
+                         'Здесь вы можете ознакомиться с основной '
+                         'информацией о сервисе.')
 # Create your views here.
+def group_posts(request):
+    return HttpResponse('Здесь будут находиться посты, '
+                         'отфильтрованные по группам.')
