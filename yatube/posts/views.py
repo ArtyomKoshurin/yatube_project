2532ce599d795ad290ad2294b4fcaa2e5be3535f
Артём <coshurin.art@yandex.ru>
2023-01-20 21:48:00 +0300

    добавлен шаблон главной страницы

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,13 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Главная страница. '
-                         'Здесь ты можещь ознакомиться с основной '
-                         'информацией о сервисе.'
-                         'Ты <i>не можешь</i> получить правильные '
-                         '<b>ответы</b>,<br> если у тебя нет '
-                         'правильных <s>вопросов</s> запросов.'
-    )
+    templates = 'posts/index.html'
+    return render(request, templates)
 # Create your views here.
 def group_posts(request, any_slug):
     return HttpResponse('Страница постов, отсортированных по группам')
