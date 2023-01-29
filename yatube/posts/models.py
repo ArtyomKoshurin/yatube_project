d601e9958468c573288e2fc2534b9d9c28a7f5ec
Артём <coshurin.art@yandex.ru>
2023-01-29 16:05:57 +0300

    adding a new model

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,6 +5,15 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -13,5 +22,10 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
-
-# Create your models here.
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def index(request):
     template = 'posts/index.html'
-    title = 'Это главная страница проекта Yatube'
+    title = 'Yatube'
+    text = 'Это главная страница проекта Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'text': title,
+        'text': text,
+        'title': title,
+        'posts': posts,
     }
     return render(request, template, context)
 # Create your views here.
@@ -13,8 +18,10 @@ def index(request):
 
 def group_posts(request):
     template = 'posts/group_list.html'
-    title = 'Здесь будет информация о группах проекта Yatube'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    title = 'Лев Толстой – зеркало русской революции'
     context = {
-        'text': title,
+        'text': text,
+        'title': title,
     }
     return render(request, template, context)
