bc256f4595e459d3ede84c6221f5c3063554cb51
Артём <coshurin.art@yandex.ru>
2023-01-29 18:38:07 +0300

    Добавлены старницы групп

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
@@ -16,12 +16,14 @@ def index(request):
 # Create your views here.
 
 
-def group_posts(request):
+def group_posts(request, any_slug):
     template = 'posts/group_list.html'
-    text = 'Здесь будет информация о группах проекта Yatube'
-    title = 'Лев Толстой – зеркало русской революции'
+    title = 'Записи сообщества'
+    group = get_object_or_404(Group, slug=any_slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'text': text,
         'title': title,
+        'group': group,
+        'posts': posts,
     }
     return render(request, template, context)
